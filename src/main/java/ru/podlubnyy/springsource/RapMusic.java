87cee3f95852b6b9d5988746ac22896dec3ec112
alexcypriiz <alexey.podlubny@yandex.ru>
2021-03-14 23:30:01 +0300

    Study 8. Bean LifeCircle(init-method, destroy-method, factory-method)

--- src/main/java/ru/podlubnyy/springsource/RapMusic.java
+++ src/main/java/ru/podlubnyy/springsource/RapMusic.java
@@ -1,8 +1,27 @@
 package ru.podlubnyy.springsource;
 
 public class RapMusic implements Music {
+    private RapMusic() {}
+    String track = "Hollywood";
+
+    public static RapMusic addRapMusic() {
+        return new RapMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
-        return "Hollywood";
+        return track;
+    }
+
+    public void setSong(String track) {
+        this.track = track;
     }
 }
--- src/main/java/ru/podlubnyy/springsource/TestSpring.java
+++ src/main/java/ru/podlubnyy/springsource/TestSpring.java
@@ -7,11 +7,21 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        RapMusic firstRapMusic = context.getBean("musicBean", RapMusic.class);
+        RapMusic secondRapMusic = context.getBean("musicBean", RapMusic.class);
+
+        secondRapMusic.setSong("All The Stars");
+        System.out.println(firstRapMusic.getSong());
+        System.out.println(secondRapMusic.getSong());
+
+
 //        Music music = context.getBean("musicBean", Music.class);
 
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
 //        Dependency Injection (DI)
+
+        /* Scope prototype
        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
@@ -27,10 +37,13 @@ public class TestSpring {
         System.out.printf("FirstMusicPlayer: %s %d \n", firstMusicPlayer.getName(), firstMusicPlayer.getVolume());
         System.out.printf("SecondMusicPlayer: %s %d \n", secondMusicPlayer.getName(), secondMusicPlayer.getVolume());
 
-        /*
+
+
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 */
+
         context.close();
     }
 }
