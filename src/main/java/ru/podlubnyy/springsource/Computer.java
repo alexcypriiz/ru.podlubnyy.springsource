1779c8c24c7303b78bc6acdaf6b4fbf35c47bac3
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-03-19 17:10:02 +0300

    Refactor/use annotation (#3)
    * refactor: use @component annotation
    
    * refactor: use @autowired annotation
    
    * refactor: use @component annotation (#2) (#5)
    
    Co-authored-by: Alexey <80638551+alexcypriiz@users.noreply.github.com>
    
    Co-authored-by: Vladislav Bukatov <vasyayp@yandex.ru>

---
+++ src/main/java/ru/podlubnyy/springsource/Computer.java
@@ -0,0 +1,19 @@
+package ru.podlubnyy.springsource;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Computer {
+    private int id;
+    private MusicPlayer musicPlayer;
+
+    public Computer(MusicPlayer musicPlayer) {
+        this.id = 1;
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer " + id + " - " + musicPlayer.playMusic();
+    }
+}
--- src/main/java/ru/podlubnyy/springsource/MusicPlayer.java
+++ src/main/java/ru/podlubnyy/springsource/MusicPlayer.java
@@ -1,38 +1,22 @@
 package ru.podlubnyy.springsource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class MusicPlayer {
-    private Music music;
+    private RapMusic rapMusic;
+    private PopMusic popMusic;
 
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
+    @Autowired
+    public MusicPlayer(RapMusic rapMusic, PopMusic popMusic) {
+        this.rapMusic = rapMusic;
+        this.popMusic = popMusic;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer() {}
-
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-            System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playning: " + rapMusic.getSong() + ". Next: " + popMusic.getSong();
     }
 }
