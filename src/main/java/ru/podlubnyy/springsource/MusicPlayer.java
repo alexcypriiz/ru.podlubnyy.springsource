8d86adffaafc3ae7b849f4c56b21a4b3e53aa90b
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-03-22 13:40:27 +0300

    refactor: use component annotation (#6)

--- src/main/java/ru/podlubnyy/springsource/MusicPlayer.java
+++ src/main/java/ru/podlubnyy/springsource/MusicPlayer.java
@@ -1,22 +1,23 @@
 package ru.podlubnyy.springsource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
-    private RapMusic rapMusic;
-    private PopMusic popMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(RapMusic rapMusic, PopMusic popMusic) {
-        this.rapMusic = rapMusic;
-        this.popMusic = popMusic;
+    public MusicPlayer(@Qualifier("rapMusic") Music music1, @Qualifier("popMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playning: " + rapMusic.getSong() + ". Next: " + popMusic.getSong();
+        return "Playning: " + music1.getSong() + ". Next: " + music2.getSong();
     }
 }
