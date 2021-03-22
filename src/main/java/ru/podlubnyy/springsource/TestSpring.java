7d572dc93f7ddba90b37376ca9fd2511bee0923d
alexcypriiz <alexey.podlubny@yandex.ru>
2021-03-22 13:39:11 +0300

    refactor: use component annotation

--- src/main/java/ru/podlubnyy/springsource/TestSpring.java
+++ src/main/java/ru/podlubnyy/springsource/TestSpring.java
@@ -8,15 +8,6 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        PopMusic popMusic = context.getBean("popMusic", PopMusic.class);
-//        RapMusic rapMusic = context.getBean("rapMusic", RapMusic.class);
-//
-//        MusicPlayer musicPlayer1 = new MusicPlayer(popMusic);
-//        musicPlayer1.playMusic();
-//
-//        MusicPlayer musicPlayer2 = new MusicPlayer(rapMusic);
-//        musicPlayer2.playMusic();
-
         Computer computer = context.getBean("computer", Computer.class);
         System.out.println(computer);
         context.close();
