cec1d16a75ab1f489bc5cb2bb18f00a4d3fe2bb0
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-03-24 18:53:28 +0300

    Use value annotation (#8)

--- src/main/java/ru/podlubnyy/springsource/RapMusic.java
+++ src/main/java/ru/podlubnyy/springsource/RapMusic.java
@@ -7,10 +7,9 @@ import java.util.List;
 
 @Component
 public class RapMusic implements Music {
-    List<String> list = Arrays.asList("Post Malone feat. 21 Savage - RockStar", "Meek Mill feat. Drake - Going Bad", "Wiz Khalifa feat. Snoop Dog - Purp and Yellow");
 
     @Override
-    public List<String> getSong() {
-        return list;
+    public String getSong() {
+        return "Wiz Khalifa feat. Snoop Dog - Purp and Yellow";
     }
 }
