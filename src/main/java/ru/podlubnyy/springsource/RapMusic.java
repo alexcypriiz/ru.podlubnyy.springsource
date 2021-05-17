64e86e9b07d588f1a7a42e43be3c377a83b004d9
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-05-17 21:21:34 +0300

    Use PostConstruct and PreDestroy annotation (#10)

--- src/main/java/ru/podlubnyy/springsource/RapMusic.java
+++ src/main/java/ru/podlubnyy/springsource/RapMusic.java
@@ -1,12 +1,24 @@
 package ru.podlubnyy.springsource;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Arrays;
 import java.util.List;
-
 @Component
 public class RapMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy");
+    }
 
     @Override
     public String getSong() {
