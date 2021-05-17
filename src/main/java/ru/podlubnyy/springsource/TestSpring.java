53e98ade07fa5bf6346651d7c0c524be4bac31f7
alexcypriiz <alexey.podlubny@yandex.ru>
2021-05-17 22:05:15 +0300

    Use Spring Java-based configuration

--- src/main/java/ru/podlubnyy/springsource/TestSpring.java
+++ src/main/java/ru/podlubnyy/springsource/TestSpring.java
@@ -1,12 +1,10 @@
 package ru.podlubnyy.springsource;
 
-import com.sun.security.jgss.GSSUtil;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
@@ -14,10 +12,10 @@ public class TestSpring {
         musicPlayer2.setName("Chill");
         musicPlayer2.setVolume(55);
 
-
         System.out.println(musicPlayer1.getName() + " " + musicPlayer1.getVolume());
-
         System.out.println(musicPlayer2.getName() + " " +musicPlayer2.getVolume());
+
+        System.out.println(musicPlayer1.playMusic());
 
         context.close();
     }
